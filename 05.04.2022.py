664b441d812e68895474f0b141688496254561ec
christofer18 <Christofer.karus@tptlive.ee>
2022-04-05 09:04:03 +0300

    Create 05.04.2022.py

---
+++ 05.04.2022.py
@@ -0,0 +1,18 @@
+map = [
+    [12, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 24]
+]
+start_x = 0
+start_y = 0
+
+def print_kaart():
+    print(rida1)
+    print(rida2)
+    print(rida3)
+    print(rida4)
+    print(rida5)
+
+print_kaart()
